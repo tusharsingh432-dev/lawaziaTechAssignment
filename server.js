d3ce0c122ce9a29aa5789c13da5378ce33e9ec25
Tushar Singh <tusharsingh432@gmail.com>
2022-05-25 17:35:59 +0530

    Transport type added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json());
 const winston = require('winston');
 
 const logger = winston.createLogger({
@@ -10,7 +10,8 @@ const logger = winston.createLogger({
     defaultMeta: { service: 'user-service' },
     transports: [
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'requests.log' }),
+        new winston.transports.File({ filename: 'requests.log', level: 'info' }),
+        new winston.transports.File({ filename: 'warnings.log', level: 'warn' })
     ],
 });
 
@@ -19,6 +20,22 @@ app.get('/request1', (req, res) => {
     logger.log('info', 'Request 1');
     res.status(200).json({
         status: 'success'
+    })
+})
+
+app.post('/request2', (req, res) => {
+    console.log(req.body);
+    const { logReq } = req.body;
+    if (!logReq) {
+        res.status(400).json({
+            status: 'error',
+            message: 'Request Should have a logReq'
+        })
+    }
+    logger.log(logReq, 'Request 2');
+    res.status(200).json({
+        status: 'success',
+        logType: logReq
     })
 })
 
